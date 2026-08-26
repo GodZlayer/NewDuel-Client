1f088e904999b11aac13b93dd43118d1b1ca9252
GodZlayer <santoss.cog@gmail.com>
2026-08-25 21:12:31 -0300

    Replace client distribution with reorganized NewDuel assets and runtime

--- check_account.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import sys
-
-user = sys.argv[1]
-db_path = sys.argv[2]
-
-con = sqlite3.connect(db_path)
-count = con.execute("SELECT COUNT(*) FROM Account WHERE UserID=?", (user,)).fetchone()[0]
-print(count)
